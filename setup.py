7714a083224e4414abf7b9617fcacd81ebc02bd0
xzt41 <xzt41@126.com>
2017-05-18 17:19:02 +0800

    test

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
 
     # https://packaging.python.org/en/latest/requirements.html
 
-    install_requires=['pysam>0.8.4','matplotlib','numpy','scipy','pyfasta','biopython','h5py'],
+    install_requires=['pysam>0.8.4','matplotlib','numpy>1.11.0','scipy','pyfasta','biopython','h5py'],
 
 
     # List additional groups of dependencies here (e.g. development
