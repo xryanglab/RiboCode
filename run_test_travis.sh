0c233e2e0d1d8e6e3b05a3e113f95275e9c05618
xzt41 <xzt41@126.com>
2017-05-18 21:50:43 +0800

    test

--- run_test_travis.sh
+++ run_test_travis.sh
@@ -4,11 +4,34 @@ pushd .
 
 WORKDIR=`pwd`
 
+#1
 echo
 echo "test prepare_transcripts"
 echo
 
 prepare_transcripts -h
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+#2
+echo
+echo "test metaplots"
+echo
+
+metaplots -h
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+#3
+echo
+echo "test plot_orf_density"
+echo
+
+plot_orf_density -h
 
 if [ $? != 0 ]; then
 	exit 1
