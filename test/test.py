21239a9f26585ced8e4ee88423a1685bebed7668
xzt41 <xzt41@126.com>
2017-05-18 21:28:57 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:UTF-8 -*-
 __author__ = 'Zhengtao Xiao'
-from RiboCode import prepare_transcripts,metaplots,plot_orf_density,RPF_count_ORF
+from RiboCode import prepare_transcripts,metaplots,plot_orf_density
 
 def test_prepare_transcripts():
 	prepare_transcripts.main()
@@ -11,6 +11,3 @@ def test_metaplots():
 
 def test_plot_orf_density():
 	plot_orf_density.main()
-
-def test_ORFcount():
-	RPF_count_ORF.main()
