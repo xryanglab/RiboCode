150d2a94d370d8c60ea4470f18d83892892e2e1d
xzt41 <xzt41@126.com>
2017-05-18 20:31:02 +0800

    test

---
+++ test/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+__author__ = 'Zhengtao Xiao'
+
+def test_prepare_transcripts():
+	RiboCode.prepare_transcripts.main()
+
+def test_metaplots():
+	RiboCode.metaplots.main()
+
+def test_plot_orf_density():
+	RiboCode.plot_orf_density.main()
+
+def test_ORFcount():
+	RiboCode.RPF_count_ORF.main()
